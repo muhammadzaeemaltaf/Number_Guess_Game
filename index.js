9333ac64957c70f555a5285d67ca4ca0a0ec3f88
muhammadzaeemaltaf <165184980+muhammadzaeemaltaf@users.noreply.github.com>
2024-04-24 16:23:47 +0500

    updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 console.log("Wellcome to Number Guessing Game");
